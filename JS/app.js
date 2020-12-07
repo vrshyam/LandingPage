32fa522e48b211982b6b012700b041553a83803e
vrshyam <vrshyamsundar@gmail.com>
2020-12-07 10:07:06 +0530

    activemenu

--- JS/app.js
+++ JS/app.js
@@ -50,3 +50,11 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll('a');
+const menuLength = menuItem.length
+for (let i = 0; i < menuLength; i++){
+    if(menuItem[i].href === currentLocation){
+        menuItem[i].className = "activemenu"
+    }
+} 
